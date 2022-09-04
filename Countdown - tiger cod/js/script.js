e5517b2df8a3be0dd0870de8a8f0d2c0610dc47f
ORichardMorais99 <102425601+ORichardMorais99@users.noreply.github.com>
2022-09-04 18:30:52 -0300

    my-personal-commit

---
+++ Countdown - tiger cod/js/script.js
@@ -0,0 +1,16 @@
+import Countdown from './countdown.js';
+
+const tempoParaOSeteDeSetembro = new Countdown("07 September 2022 20:00:00 GMT-0300");
+const tempos = document.querySelectorAll('[data-time]');
+
+function mostrarTempo(){
+    tempos.forEach((tempo, index) => {
+        tempo.innerHTML = tempoParaOSeteDeSetembro.total[index]
+    });
+}
+
+mostrarTempo();
+setInterval(mostrarTempo, 1000)
+
+
+
